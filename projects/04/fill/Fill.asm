c6a7f2300bebf0661e0e166763c3d7b5b2707c9c
Louis Le <thelouisle@gmail.com>
2021-07-06 23:32:45 -0400

    Added Fill.asm
    Finally figured out this asm syntax. Book needs to take its
    excessive explanations in some areas and put it into the areas
    like explaining the project properly.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // Reset counter to 0
+    @COUNTER
+    M = 0
+
+    // Check for keyboard input
+    @KBD
+    D = M
+
+    // Make pixel white
+    @WHITE
+    D;JEQ
+    (BLACK)
+        // if (COUNTER == 8192) goto LOOP
+        @COUNTER
+        D = M
+        @8192
+        D = D - A
+        @LOOP
+        D;JEQ
+
+        // Set pixel to black
+        @COUNTER
+        D = M
+        @SCREEN
+        A = A + D
+        M = -1
+
+        @COUNTER
+        M = M + 1
+
+        @BLACK
+        0;JMP
+
+    // Set pixel to white
+    (WHITE)
+        // if (COUNTER == 8192) goto LOOP
+        @COUNTER
+        D = M
+        @8192
+        D = D - A
+        @LOOP
+        D;JEQ
+
+        // Set pixel to white
+        @COUNTER
+        D = M
+        @SCREEN
+        A = A + D
+        M = 0
+        
+        // Increase counter
+        @COUNTER
+        M = M + 1
+
+        @WHITE
+        0;JMP
